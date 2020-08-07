173f0877597a622c448c402136e486b85512010a
Noel Kirkland <noelkirkland@gmail.com>
2020-08-07 15:49:02 -0700

    create EngineersController Index GET route

---
+++ Factory/Controllers/EngineersController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Factory.Controllers
+{
+  public class EngineersController : Controller
+  {
+    private readonly FactoryContext _db;
+
+    public EngineersController(FactoryContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      return View(_db.Engineers.ToList());
+    }
+  }
+}
