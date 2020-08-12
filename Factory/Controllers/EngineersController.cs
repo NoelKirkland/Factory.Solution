53e12b17feaf679dd4d899972e276ca958d15043
Noel Kirkland <noelkirkland@gmail.com>
2020-08-11 17:00:28 -0700

    remove the option to add a Machine when creating an Engineer. You can still make that relationship through the AddMachine or AddEngineer views

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -25,7 +25,6 @@ namespace Factory.Controllers
 
     public ActionResult Create()
     {
-      ViewBag.MachineId = new SelectList(_db.Machines, "MachineId", "MachineName");
       return View();
     }
 
@@ -33,10 +32,6 @@ namespace Factory.Controllers
     public ActionResult Create(Engineer engineer, int MachineId)
     {
       _db.Engineers.Add(engineer);
-      if (MachineId != 0)
-      {
-        _db.EngineerMachine.Add(new EngineerMachine() { MachineId = MachineId, EngineerId = engineer.EngineerId });
-      }
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
