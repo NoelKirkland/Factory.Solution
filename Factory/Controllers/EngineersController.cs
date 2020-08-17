a6017b5593534cfb47ff4c9631c26c89195a9990
Noel Kirkland <noelkirkland@gmail.com>
2020-08-16 19:25:49 -0700

    final edits for resubmission

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -1,10 +1,8 @@
 using System.Collections.Generic;
 using System.Linq;
-
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-
 using Factory.Models;
 
 namespace Factory.Controllers
